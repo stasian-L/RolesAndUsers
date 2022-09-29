d693e3d0757e82bc7a48b62187150aef933f0107
Lobach Stanislav <lobach.stanislav@inbox.ru>
2022-09-29 21:37:22 +0300

    correct mapping

--- RolesAndUsers/Profiles/UserProfile.cs
+++ RolesAndUsers/Profiles/UserProfile.cs
@@ -12,20 +12,12 @@ namespace RolesAndUsers.Profiles
                 .ForMember(dest => dest.Roles, src => src.MapFrom(src => src.UserRoles.Select(x => new RoleDto { Id = x.RoleId,Name = x.Role.Name})));
 
             CreateMap<UserDto, User>()
-                .ForMember(dest => dest.UserRoles, src => src.MapFrom(src => src.Roles)).AfterMap((model, entity) =>
-                {
-                    foreach (var userRole in entity.UserRoles)
-                    {
-                        userRole.UserId = entity.Id;
-                        userRole.User = entity;
-                    }
-                });
+                .ForMember(dest => dest.UserRoles, src => src.MapFrom(src => src.Roles));
 
             CreateMap<RoleDto, UserRole>().ForMember(dest => dest.Role, src => src.MapFrom(src => src))
                 .ForMember(dest => dest.RoleId, src => src.MapFrom(src => src.Id));
 
-            CreateMap<Role, RoleDto>().ForSourceMember(x => x.UserRoles, y => y.DoNotValidate()).ForMember(dest => dest.Name, src => src.MapFrom(src => src.Name))
-                .ForMember(dest => dest.Id, src => src.MapFrom(src => src.Id)).ReverseMap();
+            CreateMap<Role, RoleDto>().ForSourceMember(x => x.UserRoles, y => y.DoNotValidate()).ReverseMap();
         }
     }
 }
